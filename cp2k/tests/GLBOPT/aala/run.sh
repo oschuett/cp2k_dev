8b30f33df98056bd55b0f8c4b2f000dbb97fe386
Ole Schütt <ole.schuett@mat.ethz.ch>
2013-01-09 14:35:13 +0100

    moved glb_opt groudwork into git-svn based branch

---
+++ cp2k/tests/GLBOPT/aala/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ 
+mpiexec -n 5 ../../../exe/Linux-x86-64-gfortran/cp2k.popt ./aala_gbl_geo_opt.inp
+#mpiexec -n 5 ../../../exe/Linux-x86-64-gfortran/cp2k.pdbg ./aala_gbl_geo_opt.inp
+
+
+#EOF
