22a87930ede4b126305e79ce9faaf8c8740839a9
Ole Schütt <ole.schuett@mat.ethz.ch>
2013-03-06 17:58:32 +0100

    added single walker mode

--- cp2k/tests/GLBOPT/aala/run.sh
+++ cp2k/tests/GLBOPT/aala/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 #mpiexec -n 5 ../../../exe/Linux-x86-64-gfortran/cp2k.popt ./aala_gbl_geo_opt.inp
-mpiexec -n 5 ../../../exe/Linux-x86-64-gfortran/cp2k.pdbg ./aala_gbl_geo_opt.inp
+mpiexec -n 1 ../../../exe/Linux-x86-64-gfortran/cp2k.pdbg ./aala_gbl_geo_opt.inp
 
 
 #EOF
